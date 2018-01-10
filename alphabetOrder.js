ef780e1f93298a18f7e440a910f4f5d3dae1e4f5
Tina Nguyen <nguyentina2005@yahoo.com>
2018-01-10 16:30:34 -0500

    add commment about a regex code

--- alphabetOrder.js
+++ alphabetOrder.js
@@ -3,11 +3,17 @@ const should = require("chai").should();
 
 function makeAlphabet(str) { 
  if(typeof str === 'string' && str.length >= 2){
- //removeNonletter remove all character that are not letters in the string    
+    //removeNonletter remove all character that are not letters in the string
+    //  the [] represents a character (or a number of different possible characters)
+    //  the ^ means all characters EXCEPT the ones defined in the brackets
+    //  A-Z equals capital letters
+    //  a-z equals lowercase letters    
     const removeNonletters = str.replace(/[^A-Za-z]+/g, ''); 
+    
     const arr = removeNonletters.toLowerCase().split(''),
         alpha = arr.sort().join('').replace(/\s+/g, '');
-    return alpha; 
+    
+        return alpha; 
 }else{
     console.log ("string only has one letter or not a string (not letters)")
     } 
