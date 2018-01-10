ae97c56ee03ba6c1d2e3cef06bf06cfa27e3d29d
Tina Nguyen <nguyentina2005@yahoo.com>
2018-01-10 16:27:35 -0500

    removing nonletters from string

--- alphabetOrder.js
+++ alphabetOrder.js
@@ -2,10 +2,16 @@
 const should = require("chai").should();
 
 function makeAlphabet(str) { 
-    const arr = str.toLowerCase().split(''),
+ if(typeof str === 'string' && str.length >= 2){
+ //removeNonletter remove all character that are not letters in the string    
+    const removeNonletters = str.replace(/[^A-Za-z]+/g, ''); 
+    const arr = removeNonletters.toLowerCase().split(''),
         alpha = arr.sort().join('').replace(/\s+/g, '');
     return alpha; 
+}else{
+    console.log ("string only has one letter or not a string (not letters)")
+    } 
 }
 
-console.log(makeAlphabet("Tina Nguyen"));
+console.log(makeAlphabet("ab4fg12h%"));
 module.exports = makeAlphabet;
